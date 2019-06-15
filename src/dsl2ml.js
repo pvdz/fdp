929f7c232799dcf7c0b4652bd62fb23a87a85a1b
Josh Howe <josh@howe.io>
2019-06-15 18:15:14 -0400

    BUGFIX: ArrayBufferTransfer polyfill bug
    The original polyfill had a bug that made the transfer happen
    incorrectly in Firefox under certain circumstances. This is the
    corrected polyfill.

--- src/dsl2ml.js
+++ src/dsl2ml.js
@@ -1578,53 +1578,14 @@ function dsl2ml(dslStr, problem, _debug) {
 function ArrayBufferTransferPoly(source, length) {
   // c/p https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer/transfer
 
-  source = Object(source);
-  var dest = new ArrayBuffer(length);
-  if (!(source instanceof ArrayBuffer) || !(dest instanceof ArrayBuffer)) {
-    throw new TypeError('Source and destination must be ArrayBuffer instances');
-  }
-  if (dest.byteLength >= source.byteLength) {
-    var nextOffset = 0;
-    var leftBytes = source.byteLength;
-    var wordSizes = [8, 4, 2, 1];
-    wordSizes.forEach(function(_wordSize_) {
-      if (leftBytes >= _wordSize_) {
-        var done = transferWith(_wordSize_, source, dest, nextOffset, leftBytes);
-        nextOffset = done.nextOffset;
-        leftBytes = done.leftBytes;
-      }
-    });
-  }
-  return dest;
-  function transferWith(wordSize, source, dest, nextOffset, leftBytes) {
-    var ViewClass = Uint8Array;
-    switch (wordSize) {
-      case 8:
-        ViewClass = Float64Array;
-        break;
-      case 4:
-        ViewClass = Float32Array;
-        break;
-      case 2:
-        ViewClass = Uint16Array;
-        break;
-      case 1:
-        ViewClass = Uint8Array;
-        break;
-      default:
-        ViewClass = Uint8Array;
-        break;
-    }
-    var view_source = new ViewClass(source, nextOffset, Math.trunc(leftBytes / wordSize));
-    var view_dest = new ViewClass(dest, nextOffset, Math.trunc(leftBytes / wordSize));
-    for (var i = 0; i < view_dest.length; i++) {
-      view_dest[i] = view_source[i];
-    }
-    return {
-      nextOffset: view_source.byteOffset + view_source.byteLength,
-      leftBytes: source.byteLength - (view_source.byteOffset + view_source.byteLength),
-    };
-  }
+  if (!(source instanceof ArrayBuffer))
+    throw new TypeError('Source must be an instance of ArrayBuffer');
+  if (length <= source.byteLength)
+    return source.slice(0, length);
+  var sourceView = new Uint8Array(source),
+    destView = new Uint8Array(new ArrayBuffer(length));
+  destView.set(sourceView);
+  return destView.buffer;
 }
 
 // BODY_STOP
